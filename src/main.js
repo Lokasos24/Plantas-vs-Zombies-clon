7e283228937d8d1d481afc78fcc167563e46003a
Lokasos24 <tatan.lps@hotmail.com>
2026-04-14 14:20:11 -0400

    Pequeño comentario de prueba

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { Game } from './game.js';
-
+//Inicializacion de todo
 const game = new Game('gameCanvas');
 game.start();
 console.log("Sistema de módulos cargado. ¡El arquitecto está en casa!");
